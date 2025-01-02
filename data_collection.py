78f55c8b20d086ab5a1f85a2a0e83a570791713e
Bill <136879108+licarin@users.noreply.github.com>
2025-01-02 19:55:34 +0700

    init

---
+++ data_collection.py
@@ -0,0 +1,49 @@
+import os
+import cv2
+
+DATA_DIR = './data'
+if not os.path.exists(DATA_DIR):
+    os.makedirs(DATA_DIR)
+
+number_of_classes = 3
+dataset_size = 100
+
+cap = cv2.VideoCapture(0)  # Ganti indeks kamera jika perlu
+if not cap.isOpened():
+    print("Error: Camera not detected!")
+    exit()
+
+for j in range(number_of_classes):
+    class_dir = os.path.join(DATA_DIR, str(j))
+    if not os.path.exists(class_dir):
+        os.makedirs(class_dir)
+
+    print(f"Collecting data for class {j}")
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Error: Failed to capture frame.")
+            break
+
+        cv2.putText(frame, 'Ready? Press "Enter" to start ', (50, 50), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('Enter'):
+            break
+
+    counter = 0
+    while counter < dataset_size:
+        ret, frame = cap.read()
+        if not ret:
+            print("Error: Failed to capture frame.")
+            break
+
+        cv2.imshow('frame', frame)
+        cv2.waitKey(25)
+        file_path = os.path.join(class_dir, f'{counter}.jpg')
+        cv2.imwrite(file_path, frame)
+        counter += 1
+
+cap.release()
+cv2.destroyAllWindows()
